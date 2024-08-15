a8079c6db79d4b0e9e9b4929798cba58d4531756
kenjinz <Ken.Cry.K.7852396541@gmail.com>
2024-08-15 13:17:01 +0700

    testing

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { Bubble } from 'sioux-community-browse-ui-v1';
+import { BubbleSystem, ItemData } from './BubbleSystem';
+
+const data: ItemData[] = Array.from({ length: 60 }).map(() => ({
+  imgUrl: './king.svg',
+  type: 'birthday',
+}));
+function App() {
+  return (
+    <div className="w-screen h-screen overflow-hidden">
+      <BubbleSystem
+        itemSize={80}
+        items={data}
+        renderItem={({ imgUrl, type, focused, workAnniversaryYears }) => (
+          <Bubble
+            imageUrl={imgUrl}
+            type={type}
+            focused={focused}
+            workAnniversaryYears={workAnniversaryYears}
+          />
+        )}
+      />
+    </div>
+  );
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,29 +3,12 @@ import { createRoot } from 'react-dom/client';
 
 import './index.css';
 import 'sioux-community-browse-ui-v1/dist/style.css';
-import { BubbleSystem } from './BubbleSystem';
-import { Bubble } from 'sioux-community-browse-ui-v1';
-import { ItemData } from 'sioux-community-bubbles-component';
 
-const data: ItemData[] = Array.from({ length: 60 }).map(() => ({
-  imgUrl: './king.svg',
-  type: 'birthday',
-}));
+import App from './App';
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div className="w-screen h-screen overflow-hidden">
-      <BubbleSystem
-        itemSize={80}
-        items={data}
-        renderItem={({ imgUrl, type, focused, workAnniversaryYears }) => (
-          <Bubble
-            imageUrl={imgUrl}
-            type={type}
-            focused={focused}
-            workAnniversaryYears={workAnniversaryYears}
-          />
-        )}
-      />
-    </div>
+    <App />
   </StrictMode>
 );
